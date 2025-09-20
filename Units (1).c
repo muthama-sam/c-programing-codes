c8dc072fa2663e793f8b4d3b47c9ac23f3619208
muthama-sam <muthamasam06@gmail.com>
2025-09-20 16:18:21 +0300

    Add files via upload

---
+++ Units (1).c
@@ -0,0 +1,21 @@
+//simple c programming (hello world)single line comment
+/*
+Name:Samuel Muthama 
+Reg No:CT101/G/26523/25
+Date:17/9/2025
+Description:UNITS
+*/
+#include<stdio.h>
+
+int main(){
+    
+    printf("Introduction to programming and problem solving\n");
+    printf("Physics\n");
+    printf("Mathematics for science\n");
+    printf("Computer architecture \n");
+    printf("Analogue electronics\n");
+    printf("Descrete mathematics\n");
+    printf("Fundamental of ICT\n");
+    printf("Communication skills");
+    return 0;
+    }
---
+++ prompt for entry .c
@@ -0,0 +1,26 @@
+//simple c programming (hello world)single line comment
+/*
+Name:Samuel Muthama
+Reg No:CT101/G/26523/25
+Date:17/9/2025
+Description:prompt for entry
+*/
+#include<stdio.h>
+
+int main(){
+int height;
+    printf("Enter your height:\n");
+    scanf("%d",&height);
+    printf("your height is %dM,height\n");
+    
+int bankbalance;    
+    printf("Enter your bankbalance:\n");
+    scanf("%d",&bankbalance);
+    printf("your bankbalance is ksh.%d,bankbalance\n");
+    
+int phonenumber;
+    printf("Enter your phonenumber:\n");
+    scanf("%d",phonenumber);
+    printf("your phonenumber is %d,phonenumber");
+    return 0;
+    }
