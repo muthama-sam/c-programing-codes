3dddef9c914a8fdf06466015f6811b6c1315dac6
muthama-sam <muthamasam06@gmail.com>
2025-10-09 10:04:50 +0300

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,18 @@
+//simple c programming (hello world)single line comment
+/*
+Name:Samuel Muthama
+Reg No:CT101/G/26523/25
+Date:8/10/2025
+Description:for_loop
+*/
+#include <stdio.h>
+int main (){
+
+int i=100;
+    for(i<=100; i>=50; i--){
+    printf("%d\n",i);
+    }
+
+    return 0;
+
+}
